dc7be47c851b5eb354ab78441064052fae559250
Evgeny Tishkov <evgeny.tishkov@fishplace.ru>
2025-01-23 21:52:21 +1000

    Добавил структуру проекта и webpack сборку

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "./build"),
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+        }),
+    ],
+    devServer: {
+        watchFiles: path.join(__dirname, "src"),
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+};
